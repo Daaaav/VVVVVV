38505e2f3209c29d3ef0a8af0e787001d9142277
Dav999-v <dav999.tolp@gmail.com>
2021-12-10 17:52:20 +0100

    Untabify Textbook
    Oops, my editor was only set to indent C++ files with spaces, not C
    files.

--- desktop_version/src/Textbook.c
+++ desktop_version/src/Textbook.c
@@ -6,98 +6,98 @@
 
 void textbook_init(Textbook* textbook)
 {
-	textbook->pages_used = 0;
+    textbook->pages_used = 0;
 }
 
 void textbook_clear(Textbook* textbook)
 {
-	short p;
-	for (p = 0; p < textbook->pages_used; p++)
-	{
-		SDL_free(textbook->page[p]);
-	}
-	textbook->pages_used = 0;
+    short p;
+    for (p = 0; p < textbook->pages_used; p++)
+    {
+        SDL_free(textbook->page[p]);
+    }
+    textbook->pages_used = 0;
 }
 
 const char* textbook_store_raw(Textbook* textbook, const char* data, size_t data_len)
 {
-	short found_page = -1;
-	short p;
+    short found_page = -1;
+    short p;
 
-	if (data == NULL)
-	{
-		return NULL;
-	}
+    if (data == NULL)
+    {
+        return NULL;
+    }
 
-	if (data_len > TEXTBOOK_PAGE_SIZE)
-	{
-		vlog_warn(
-			"Cannot store data of %ld bytes in Textbook, max page size is %d",
-			data_len,
-			TEXTBOOK_PAGE_SIZE
-		);
-		return NULL;
-	}
+    if (data_len > TEXTBOOK_PAGE_SIZE)
+    {
+        vlog_warn(
+            "Cannot store data of %ld bytes in Textbook, max page size is %d",
+            data_len,
+            TEXTBOOK_PAGE_SIZE
+        );
+        return NULL;
+    }
 
-	/* Find a suitable page to place our text on */
-	for (p = 0; p < textbook->pages_used; p++)
-	{
-		size_t free = TEXTBOOK_PAGE_SIZE - textbook->page_len[p];
+    /* Find a suitable page to place our text on */
+    for (p = 0; p < textbook->pages_used; p++)
+    {
+        size_t free = TEXTBOOK_PAGE_SIZE - textbook->page_len[p];
 
-		if (data_len <= free)
-		{
-			found_page = p;
-			break;
-		}
-	}
+        if (data_len <= free)
+        {
+            found_page = p;
+            break;
+        }
+    }
 
-	if (found_page == -1)
-	{
-		/* Create a new page then */
-		found_page = textbook->pages_used;
+    if (found_page == -1)
+    {
+        /* Create a new page then */
+        found_page = textbook->pages_used;
 
-		if (found_page >= TEXTBOOK_MAX_PAGES)
-		{
-			vlog_warn(
-				"Textbook is full! %hd pages used (%d chars per page)",
-				textbook->pages_used,
-				TEXTBOOK_PAGE_SIZE
-			);
-			return NULL;
-		}
+        if (found_page >= TEXTBOOK_MAX_PAGES)
+        {
+            vlog_warn(
+                "Textbook is full! %hd pages used (%d chars per page)",
+                textbook->pages_used,
+                TEXTBOOK_PAGE_SIZE
+            );
+            return NULL;
+        }
 
-		textbook->page[found_page] = (char*) SDL_malloc(TEXTBOOK_PAGE_SIZE);
-		if (textbook->page[found_page] == NULL)
-		{
-			return NULL;
-		}
+        textbook->page[found_page] = (char*) SDL_malloc(TEXTBOOK_PAGE_SIZE);
+        if (textbook->page[found_page] == NULL)
+        {
+            return NULL;
+        }
 
-		textbook->page_len[found_page] = 0;
-		textbook->pages_used++;
-	}
+        textbook->page_len[found_page] = 0;
+        textbook->pages_used++;
+    }
 
-	{
-		size_t cursor = textbook->page_len[found_page];
-		char* added_text = &textbook->page[found_page][cursor];
-		SDL_memcpy(added_text, data, data_len);
-		textbook->page_len[found_page] += data_len;
+    {
+        size_t cursor = textbook->page_len[found_page];
+        char* added_text = &textbook->page[found_page][cursor];
+        SDL_memcpy(added_text, data, data_len);
+        textbook->page_len[found_page] += data_len;
 
-		return added_text;
-	}
+        return added_text;
+    }
 }
 
 const char* textbook_store(Textbook* textbook, const char* text)
 {
-	if (text == NULL)
-	{
-		return NULL;
-	}
+    if (text == NULL)
+    {
+        return NULL;
+    }
 
-	if (text[0] == '\0')
-	{
-		/* Don't go and store a single null terminator when we have one right here for you: */
-		return "";
-	}
+    if (text[0] == '\0')
+    {
+        /* Don't go and store a single null terminator when we have one right here for you: */
+        return "";
+    }
 
-	return textbook_store_raw(textbook, text, SDL_strlen(text)+1);
+    return textbook_store_raw(textbook, text, SDL_strlen(text)+1);
 }
