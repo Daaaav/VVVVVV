2da9dd4f5708878c97106b889405352ae7815093
Dav999-v <dav999.tolp@gmail.com>
2022-03-23 03:15:47 +0100

    Don't indent namespace loc blocks
    This was suggested during code review. You should probably diff this
    with --ignore-all-space.

--- desktop_version/src/LocalizationStorage.h
+++ desktop_version/src/LocalizationStorage.h
@@ -26,65 +26,67 @@ extern "C"
 
 namespace loc
 {
-    #if defined(LOCALIZATION_CPP) || defined(LOCALIZATIONSTORAGE_CPP) || defined(LOCALIZATIONMAINT_CPP)
-        LS_INTERN Textbook textbook_main;
-        LS_INTERN Textbook textbook_custom;
 
-        LS_INTERN hashmap* map_translation;
-        LS_INTERN hashmap* map_translation_plural;
-        LS_INTERN std::string number[102]; /* [101] for Lots */
-        LS_INTERN unsigned char number_plural_form[200]; /* [0..99] for 0..99, [100..199] for *00..*99 */
-        LS_INTERN hashmap* map_translation_cutscene;
-        LS_INTERN hashmap* map_translation_cutscene_custom;
-        LS_INTERN hashmap* map_translation_roomnames_special;
+#if defined(LOCALIZATION_CPP) || defined(LOCALIZATIONSTORAGE_CPP) || defined(LOCALIZATIONMAINT_CPP)
+    LS_INTERN Textbook textbook_main;
+    LS_INTERN Textbook textbook_custom;
 
-        #define MAP_MAX_X 54
-        #define MAP_MAX_Y 56
-        #define CUSTOM_MAP_MAX_X 19
-        #define CUSTOM_MAP_MAX_Y 19
-        LS_INTERN const char* translation_roomnames[MAP_MAX_Y+1][MAP_MAX_X+1];
-        LS_INTERN const char* explanation_roomnames[MAP_MAX_Y+1][MAP_MAX_X+1];
-        LS_INTERN const char* translation_roomnames_custom[CUSTOM_MAP_MAX_Y+1][CUSTOM_MAP_MAX_X+1];
-        LS_INTERN const char* explanation_roomnames_custom[CUSTOM_MAP_MAX_Y+1][CUSTOM_MAP_MAX_X+1];
-    #endif
+    LS_INTERN hashmap* map_translation;
+    LS_INTERN hashmap* map_translation_plural;
+    LS_INTERN std::string number[102]; /* [101] for Lots */
+    LS_INTERN unsigned char number_plural_form[200]; /* [0..99] for 0..99, [100..199] for *00..*99 */
+    LS_INTERN hashmap* map_translation_cutscene;
+    LS_INTERN hashmap* map_translation_cutscene_custom;
+    LS_INTERN hashmap* map_translation_roomnames_special;
 
-
-    struct TextOverflow
-    {
-        std::string lang;
-        const char* text;
-        unsigned short max_w, max_h;
-        unsigned short max_w_px, max_h_px;
-        bool multiline;
-    };
-
-    extern std::vector<TextOverflow> text_overflows;
+    #define MAP_MAX_X 54
+    #define MAP_MAX_Y 56
+    #define CUSTOM_MAP_MAX_X 19
+    #define CUSTOM_MAP_MAX_Y 19
+    LS_INTERN const char* translation_roomnames[MAP_MAX_Y+1][MAP_MAX_X+1];
+    LS_INTERN const char* explanation_roomnames[MAP_MAX_Y+1][MAP_MAX_X+1];
+    LS_INTERN const char* translation_roomnames_custom[CUSTOM_MAP_MAX_Y+1][CUSTOM_MAP_MAX_X+1];
+    LS_INTERN const char* explanation_roomnames_custom[CUSTOM_MAP_MAX_Y+1][CUSTOM_MAP_MAX_X+1];
+#endif
 
 
-    bool load_lang_doc(
-        const std::string& cat,
-        tinyxml2::XMLDocument& doc,
-        const std::string& langcode = lang,
-        const std::string& asset_cat = ""
-    );
+struct TextOverflow
+{
+    std::string lang;
+    const char* text;
+    unsigned short max_w, max_h;
+    unsigned short max_w_px, max_h_px;
+    bool multiline;
+};
 
-    char form_for_count(int n);
+extern std::vector<TextOverflow> text_overflows;
 
-    void resettext_custom(void);
-    void unloadtext_custom(void);
 
-    bool store_roomname_translation(bool custom_level, int roomx, int roomy, const char* tra, const char* explanation);
+bool load_lang_doc(
+    const std::string& cat,
+    tinyxml2::XMLDocument& doc,
+    const std::string& langcode = lang,
+    const std::string& asset_cat = ""
+);
 
-    bool fix_room_coords(bool custom_level, int* roomx, int* roomy);
+char form_for_count(int n);
 
-    void loadtext(bool check_max);
-    void loadtext_custom(const char* custom_path);
-    void loadlanguagelist(void);
+void resettext_custom(void);
+void unloadtext_custom(void);
 
-    const char* map_lookup_text(hashmap* map, const char* eng, const char* fallback);
+bool store_roomname_translation(bool custom_level, int roomx, int roomy, const char* tra, const char* explanation);
 
-    char* add_disambiguator(char disambiguator, const char* original_string, size_t* ext_alloc_len);
-}
+bool fix_room_coords(bool custom_level, int* roomx, int* roomy);
+
+void loadtext(bool check_max);
+void loadtext_custom(const char* custom_path);
+void loadlanguagelist(void);
+
+const char* map_lookup_text(hashmap* map, const char* eng, const char* fallback);
+
+char* add_disambiguator(char disambiguator, const char* original_string, size_t* ext_alloc_len);
+
+} /* namespace loc */
 
 #undef LS_INTERN
 
