7f21d66af7be64eaf83248040d28ea1899d30bb5
Dav999-v <dav999.tolp@gmail.com>
2022-08-15 03:46:17 +0200

    Prevent F12 hotkey (reload language files) from being held down
    You could hold down F12 and it would start repeatedly reloading
    language files. This is now fixed so it only works once until you lift
    the key and press it again. It's a debug key so it was pretty
    inconsequential, but it's an easy fix.
    
    https://youtu.be/tXhhKA5RtDA?t=1924
    (I didn't hear the repeated coin sounds at the time because it wasn't
    passed via the voice chat, but it became clear soon enough lol)

--- desktop_version/src/KeyPoll.cpp
+++ desktop_version/src/KeyPoll.cpp
@@ -166,7 +166,7 @@ void KeyPoll::Poll(void)
                 fullscreenkeybind = true;
             }
 
-            if (loc::show_translator_menu && evt.key.keysym.sym == SDLK_F12)
+            if (loc::show_translator_menu && evt.key.keysym.sym == SDLK_F12 && !evt.key.repeat)
             {
                 /* Reload language files */
                 loc::loadtext(false);
